3649ec92f15adc3c95126283946d341eb7d60d56
Martin Lanter <lanterm@student.ethz.ch>
2013-11-19 05:12:12 +0100

    Revomed an obsolete import statement

--- src/main/java/ch/ethz/inf/vs/scandium/util/ScandiumLogger.java
+++ src/main/java/ch/ethz/inf/vs/scandium/util/ScandiumLogger.java
@@ -4,7 +4,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.StreamHandler;
